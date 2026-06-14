f2f601e37977fab36c3b3e9ff6ffefb934e76e7d
ismailkendil058 <your-email@example.com>
2026-06-14 17:44:12 +0100

    opacity

--- src/pages/MedecinDashboard.tsx
+++ src/pages/MedecinDashboard.tsx
@@ -446,7 +446,7 @@ const MedecinDashboard = () => {
     left: 50%;
     width: 115mm;
     height: auto;
-    opacity: 0.06;
+    opacity: 0.03;
     transform: translate(-50%, -50%);
     z-index: 0;
     pointer-events: none;
